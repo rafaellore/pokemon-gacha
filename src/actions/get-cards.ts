4bf405b78124013bf9007406ed8b2c6fd36a1d52
rafaellore <birdneon3@gmail.com>
2024-09-17 16:48:11 -0300

    refact(get-cards): improves the randomizing cards

--- src/actions/get-cards.ts
+++ src/actions/get-cards.ts
@@ -2,52 +2,41 @@ import { CardSet } from "./get-sets";
 import apiError from "./utils/api-error";
 
 export interface PokemonCard {
-  id: string; // Identificador único da carta
-  name: string; // Nome do Pokémon
-  supertype: string; // Supertipo, como "Pokémon", "Trainer", "Energy"
-  subtypes: string[]; // Subtipos, como "Basic", "Stage 1", "Stage 2"
-  hp?: string; // Pontos de vida (HP) do Pokémon
-  types?: string[]; // Tipos de energia associados, como "Fire", "Water"
-  evolvesFrom?: string; // Nome do Pokémon do qual este evolui
-  abilities?: string[]; // Lista de habilidades especiais da carta
-  attacks?: string[]; // Lista de ataques disponíveis na carta
-  weaknesses?: string[]; // Lista de fraquezas do Pokémon
-  resistances?: string[]; // Lista de resistências do Pokémon
-  retreatCost?: string[]; // Custo de recuo da carta
-  set: CardSet; // Informações sobre o set ao qual a carta pertence
-  number: string; // Número da carta dentro do set
-  artist: string; // Artista que ilustrou a carta
-  rarity: string; // Raridade da carta, como "Common", "Uncommon", "Rare"
-  flavorText?: string; // Texto flavor, se disponível
-  nationalPokedexNumbers: number[]; // Números da Pokédex nacional
-  legalities: string[]; // Legalidade da carta em diferentes formatos de jogo
+  id: string;
+  name: string;
+  supertype: string;
+  subtypes: string[];
+  hp?: string;
+  types?: string[];
+  evolvesFrom?: string;
+  abilities?: string[];
+  attacks?: string[];
+  weaknesses?: string[];
+  resistances?: string[];
+  retreatCost?: string[];
+  set: CardSet;
+  number: string;
+  artist: string;
+  rarity: string;
+  flavorText?: string;
+  nationalPokedexNumbers: number[];
+  legalities: string[];
   images: {
-    small: string; // URL da imagem pequena da carta
-    large: string; // URL da imagem grande da carta
-  }; // URLs das imagens da carta
+    small: string;
+    large: string;
+  };
 }
 
-const API_URL = "https://api.pokemontcg.io/v2/cards";
-
-async function getCardsByRarity(rarity: string, count: number) {
-  const response = await fetch(`${API_URL}?q=rarity:${rarity}`, {
-    method: "GET",
-    headers: {
-      "X-Api-Key": process.env.NEXT_PUBLIC_POKEMONTCG_API_KEY,
-    } as HeadersInit,
-  });
-
-  const data = await response.json();
-
-  const cards = data.data as PokemonCard[];
-
-  // Randomiza e seleciona 'count' cartas
-  return cards.sort(() => 0.5 - Math.random()).slice(0, count);
+// Define o tipo de resposta esperada para a API de raridades
+interface RarityResponse {
+  data: {
+    id: string;
+    name: string;
+  }[];
 }
 
 export default async function getCards(setId: string) {
   try {
-    // Função auxiliar para fazer a requisição e retornar cartas por raridade
     const fetchCardsByRarity = async (rarity: string, count: number) => {
       const response = await fetch(
         `https://api.pokemontcg.io/v2/cards?q=set.id:${setId}+rarity:${rarity}`,
@@ -67,17 +56,13 @@ export default async function getCards(setId: string) {
       const data = await response.json();
       const cards = data.data as PokemonCard[];
 
-      // Randomizar e selecionar 'count' cartas
       return cards.sort(() => 0.5 - Math.random()).slice(0, count);
     };
 
-    // Buscar cartas conforme as raridades típicas de um booster pack
     const rareCards = await fetchCardsByRarity("Rare", 1);
     const uncommonCards = await fetchCardsByRarity("Uncommon", 3);
-    const commonCards = await fetchCardsByRarity("Common", 5);
+    const commonCards = await fetchCardsByRarity("Common", 3);
 
-    // Concatenar todas as cartas para formar o booster pack
-    // Ajustar a ordem: comum -> incomum -> raro
     const boosterPack = [...commonCards, ...uncommonCards, ...rareCards];
 
     return { data: boosterPack, ok: true, error: "" };
